27f69a84807ab85ee6309fd8a542b4df179243ac
thammaratrud <thammarat.rud@gmail.com>
2016-12-15 13:56:33 +0700

    add product

---
+++ modules/products/server/models/product.server.model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Product Schema
+ */
+var ProductSchema = new Schema({
+  name: {
+    type: String,
+    default: '',
+    required: 'Please fill Product name',
+    unique: true,
+    trim: true
+  },
+  description:String,
+  images:[String],
+  category: {
+    type: String,
+    required: 'Please fill Product category',
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: 'Please fill Product price'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+mongoose.model('Product', ProductSchema);
