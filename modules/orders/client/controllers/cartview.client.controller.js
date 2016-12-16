96ae84dc26a9f162cf630f3b1ede1890e2511314
nattawee <nongnatawee@hotmail.com>
2016-12-16 19:27:31 +0700

    cartview

--- modules/orders/client/controllers/cartview.client.controller.js
+++ modules/orders/client/controllers/cartview.client.controller.js
@@ -1,14 +1,15 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('orders')
     .controller('CartviewController', CartviewController);
 
-  CartviewController.$inject = ['$scope'];
+  CartviewController.$inject = ['$scope', 'ShopCartService'];
 
-  function CartviewController($scope) {
+  function CartviewController($scope, ShopCartService) {
     var vm = this;
+    vm.cart = ShopCartService.cart;
 
     // Cartview controller logic
     // ...
