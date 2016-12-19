77ebee5f328a2fecb30e3fbda8f17353a13b4ef1
Bu4y <amnes1a@windowslive.com>
2016-12-19 19:41:47 +0700

    success

--- modules/orders/tests/client/checkout-login.client.controller.tests.js
+++ modules/orders/tests/client/checkout-login.client.controller.tests.js
@@ -8,7 +8,9 @@
       $scope,
       $httpBackend,
       $stateParams,
-      $location;
+      $location,
+      mockOrder,
+      OrdersService;
 
     // The $resource service augments the response object with methods for updating and deleting the resource.
     // If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
@@ -35,7 +37,7 @@
     // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
     // This allows us to inject a service but then attach it to a variable
     // with the same name as the service.
-    beforeEach(inject(function ($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
+    beforeEach(inject(function ($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_, _OrdersService_) {
       // Set a new global scope
       $scope = $rootScope.$new();
 
@@ -43,24 +45,86 @@
       $stateParams = _$stateParams_;
       $httpBackend = _$httpBackend_;
       $location = _$location_;
-     
+      OrdersService = _OrdersService_;
+
+      mockOrder = new OrdersService({
+        _id: '525a8422f6d0f87f0e407a33',
+        name: 'Order Name'
+      });
       // Initialize the Checkout login controller.
       CheckoutLoginController = $controller('CheckoutLoginController', {
         $scope: $scope
       });
     }));
 
-    it('Should do some controller test', inject(function () {
-      // The test logic
-      // ...
+
+    describe('Signup', function () {
       it('Should be Step first', function () {
-        expect($scope.vm.step).toBe(1);
+        expect($scope.step).toBe(1);
       });
-      
-      it('Should be Next Step', function () {
-        $scope.vm.checkStep();
-        expect($scope.vm.step).toBe(2);
+
+      it('Should be Next Step not signin not email', function () {
+        $scope.checkStep(false);
+        expect($scope.step).toBe(1);
       });
-    }));
+
+      it('Should be Next Step not signin has email', function () {
+        $scope.checkStep(true);
+        expect($scope.step).toBe(2);
+      });
+
+      it('Should be Next Step not signin not first name last name and address', function () {
+        $scope.step = 2;
+        $scope.checkStep(false);
+        expect($scope.step).toBe(2);
+      });
+
+      // it('Should be Next Step not signin has first name last name and address', function () {
+      //   $scope.step = 2;
+      //   $scope.checkStep(true);
+      //   expect($scope.step).toBe(3);
+      // });
+
+      it('should register with correct data', function () {
+        // Test expected GET request
+        $scope.step = 2;
+        $scope.authentication.user = 'Fred';
+        $httpBackend.when('POST', '/api/auth/signup').respond(200, 'Fred');
+
+        $scope.signup(true);
+        $httpBackend.flush();
+        expect($scope.step).toBe(3);
+        // test scope value
+        expect($scope.authentication.user).toBe('Fred');
+        expect($scope.error).toEqual(null);
+      });
+
+      it('should login with a correct user and password', function () {
+        // Test expected GET request
+        $scope.step = 2;
+        $httpBackend.when('POST', '/api/auth/signin').respond(200, 'Fred');
+
+        $scope.signin(true);
+        $httpBackend.flush();
+        expect($scope.step).toBe(3);
+        // Test scope value
+        expect($scope.authentication.user).toEqual('Fred');
+      });
+
+      describe('vm.save() as create', function () {
+        it('should send a POST request with the form input values and then locate to new object URL', inject(function (OrdersService) {
+          // Set POST response
+          var sampleOrderPaostData = {};
+          
+          sampleOrderPaostData.docno = '1234';
+          $httpBackend.expectPOST('api/orders', sampleOrderPaostData).respond(mockOrder);
+
+          // Run controller functionality
+          $scope.saveOrder(true);
+          $httpBackend.flush();
+        }));
+      });
+    });
+
   });
 } ());
