fdc3a8c56a7eb22313196244a592edcff3b8cb43
moomam6934 <destinationpainbm@gmail.com>
2016-12-20 16:50:00 +0700

    CRUD postcode

--- config/lib/express.js
+++ config/lib/express.js
@@ -44,6 +44,13 @@ module.exports.initLocalVariables = function (app) {
     res.locals.url = req.protocol + '://' + req.headers.host + req.originalUrl;
     next();
   });
+
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 };
 
 /**
