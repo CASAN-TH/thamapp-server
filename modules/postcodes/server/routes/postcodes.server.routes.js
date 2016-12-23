22b02313baeee973ba3264b737229eb009bc0920
Bu4y <amnes1a@windowslive.com>
2016-12-23 18:09:53 +0700

    push heroku and git

--- modules/postcodes/server/routes/postcodes.server.routes.js
+++ modules/postcodes/server/routes/postcodes.server.routes.js
@@ -8,11 +8,11 @@ var postcodesPolicy = require('../policies/postcodes.server.policy'),
 
 module.exports = function(app) {
   // Postcodes Routes
-  app.route('/api/postcodes')//.all(postcodesPolicy.isAllowed)
+  app.route('/api/postcodes').all(postcodesPolicy.isAllowed)
     .get(postcodes.list)
     .post(postcodes.create);
 
-  app.route('/api/postcodes/:postcodeId')//.all(postcodesPolicy.isAllowed)
+  app.route('/api/postcodes/:postcodeId').all(postcodesPolicy.isAllowed)
     .get(postcodes.read)
     .put(postcodes.update)
     .delete(postcodes.delete);
