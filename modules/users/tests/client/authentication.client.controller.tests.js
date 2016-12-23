3dd2489ca887761a3f6ddf65ebe2d6d3e0384505
Bu4y <amnes1a@windowslive.com>
2016-12-23 10:43:40 +0700

    autocomplete in signup

--- modules/users/tests/client/authentication.client.controller.tests.js
+++ modules/users/tests/client/authentication.client.controller.tests.js
@@ -9,6 +9,7 @@
       $httpBackend,
       $stateParams,
       $state,
+      PostcodesService,
       $location;
 
     beforeEach(function () {
@@ -32,7 +33,7 @@
       // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
       // This allows us to inject a service but then attach it to a variable
       // with the same name as the service.
-      beforeEach(inject(function ($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
+      beforeEach(inject(function ($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_, _PostcodesService_) {
         // Set a new global scope
         scope = $rootScope.$new();
 
@@ -40,15 +41,31 @@
         $stateParams = _$stateParams_;
         $httpBackend = _$httpBackend_;
         $location = _$location_;
+        PostcodesService = _PostcodesService_;
 
         // Initialize the Authentication controller
         AuthenticationController = $controller('AuthenticationController', {
           $scope: scope
         });
       }));
+      describe('$scope.readPostcode()', function (PostcodesService) {
 
-      describe('$scope.signin()', function () {
         it('should login with a correct user and password', function () {
+          // Test expected GET request
+          $httpBackend.when('GET', '/api/postcodes').respond(200);
+
+          scope.readPostcode(true);
+          $httpBackend.flush();
+
+          // Test scope value
+          expect(scope.postcode).toEqual(!undefined);
+          // expect($location.url()).toEqual('/');
+        });
+
+      });
+      describe('$scope.signin()', function (_PostcodesService_) {
+
+        it('should login with a correct user and password', function (PostcodesService) {
           // Test expected GET request
           $httpBackend.when('POST', '/api/auth/signin').respond(200, 'Fred');
 
@@ -86,7 +103,7 @@
 
           }));
 
-        it('should fail to log in with nothing', function () {
+        it('should fail to log in with nothing', function (PostcodesService) {
           // Test expected POST request
           $httpBackend.expectPOST('/api/auth/signin').respond(400, {
             'message': 'Missing credentials'
@@ -99,7 +116,7 @@
           expect(scope.error).toEqual('Missing credentials');
         });
 
-        it('should fail to log in with wrong credentials', function () {
+        it('should fail to log in with wrong credentials', function (PostcodesService) {
           // Foo/Bar combo assumed to not exist
           scope.authentication.user = 'Foo';
           scope.credentials = 'Bar';
@@ -117,8 +134,8 @@
         });
       });
 
-      describe('$scope.signup()', function () {
-        it('should register with correct data', function () {
+      describe('$scope.signup()', function (_PostcodesService_) {
+        it('should register with correct data', function (PostcodesService) {
           // Test expected GET request
           scope.authentication.user = 'Fred';
           $httpBackend.when('POST', '/api/auth/signup').respond(200, 'Fred');
@@ -132,7 +149,7 @@
           expect($location.url()).toBe('/');
         });
 
-        it('should fail to register with duplicate Username', function () {
+        it('should fail to register with duplicate Username', function (PostcodesService) {
           // Test expected POST request
           $httpBackend.when('POST', '/api/auth/signup').respond(400, {
             'message': 'Username already exists'
@@ -170,4 +187,4 @@
       });
     });
   });
-}());
+} ());
