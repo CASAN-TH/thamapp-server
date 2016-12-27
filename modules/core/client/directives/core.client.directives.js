bb58b6617d1ae17b8ce7322a9a007ac2ab6b5567
Bu4y <amnes1a@windowslive.com>
2016-12-27 16:58:23 +0700

    edit directives autocomplete

--- modules/core/client/directives/core.client.directives.js
+++ modules/core/client/directives/core.client.directives.js
@@ -48,7 +48,7 @@ angular.module('core')
         typeaheadCallback: "="
       },
       link: function (scope, elem, attrs) {
-        var template = '<div class="dropdown"><ul class="dropdown-menu" style="display:block; float: left; height: auto; max-height: 200px;	width: auto; overflow-y: scroll;" ng-if="ngModel.length >= 3" ng-hide="!ngModel.length || !filitered.length || selected"><li ng-repeat="item in filitered = (typeahead | filter:{postcode:ngModel}) track by $index" ng-click="click(item)" style="cursor:pointer" ng-class="{active:$index==active}" ng-mouseenter="mouseenter($index)"><a>{{item.subdistrict}} {{item.district}} {{item.province}} {{item.postcode}}</a></li></ul></div>';
+        var template = '<div class="dropdown"></div>';
 
         elem.bind('blur', function () {
           $timeout(function () {
@@ -65,9 +65,7 @@ angular.module('core')
             scope.$digest();
           } else if ($event.keyCode === 13) { // enter
             scope.$apply(function () {
-              if (scope.active) {
-                scope.click(scope.filitered[scope.active]);
-              }
+              scope.click(scope.filitered[scope.active]);
             });
           }
         });
@@ -86,6 +84,10 @@ angular.module('core')
         };
 
         scope.$watch('ngModel', function (input) {
+          if (scope.ngModel.length >= 3) {
+            var template = '<div class="dropdown"><ul class="dropdown-menu" style="display:block; float: left; height: auto; max-height: 200px;	width: auto; overflow-y: scroll;" ng-hide="!ngModel.length || !filitered.length || selected"><li ng-repeat="item in filitered = (typeahead | filter:{postcode:ngModel}) track by $index" ng-click="click(item)" style="cursor:pointer" ng-class="{active:$index==active}" ng-mouseenter="mouseenter($index)"><a>{{item.subdistrict}} {{item.district}} {{item.province}} {{item.postcode}}</a></li></ul></div>';
+          }
+
           if (scope.selected && scope.selected.postcode === input) {
             return;
           }
