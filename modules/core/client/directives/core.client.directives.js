26c5f37ddc1c80e6f99c7d8fe0db78322a5575b8
Bu4y <amnes1a@windowslive.com>
2016-12-27 18:16:16 +0700

    edit directives autocomplete

--- modules/core/client/directives/core.client.directives.js
+++ modules/core/client/directives/core.client.directives.js
@@ -48,7 +48,7 @@ angular.module('core')
         typeaheadCallback: "="
       },
       link: function (scope, elem, attrs) {
-        var template = '<div class="dropdown"><ul class="dropdown-menu" ng-if="ngModel.length >= 3" style="display:block; float: left; height: auto; max-height: 200px;	width: auto; overflow-y: scroll;" ng-hide="!ngModel.length || !filitered.length || selected"><li ng-repeat="item in filitered = (typeahead | filter:ngModel) track by $index" ng-click="click(item)" style="cursor:pointer" ng-class="{active:$index==active}" ng-mouseenter="mouseenter($index)"><a>{{item.subdistrict}} {{item.district}} {{item.province}} {{item.postcode}}</a></li></ul></div>';
+        var template = '<div class="dropdown"><ul class="dropdown-menu" ng-if="ngModel.length >= 3" style="display:block; float: left; height: auto; max-height: 200px;	width: auto; overflow-y: scroll;" ng-hide="!ngModel.length || !filitered.length || selected"><li ng-repeat="item in filitered = (typeahead | filter:{postcode:ngModel}) track by $index" ng-click="click(item)" style="cursor:pointer" ng-class="{active:$index==active}" ng-mouseenter="mouseenter($index)"><a>{{item.subdistrict}} {{item.district}} {{item.province}} {{item.postcode}}</a></li></ul></div>';
         var filterCustom = [];
         elem.bind('blur', function () {
           $timeout(function () {
@@ -66,7 +66,7 @@ angular.module('core')
           } else if ($event.keyCode === 13) { // enter
             filterCustom = [];
             scope.typeahead.forEach(function (e) {
-              if (e.postcode.indexOf(scope.ngModel)) {
+              if (e.postcode === scope.ngModel) {
                 filterCustom.push(e);
               }
             });
