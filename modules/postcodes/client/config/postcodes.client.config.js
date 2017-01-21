9a77d007e6f7978e5b1ecd17a7804d8c150b63f7
jigkoh3 <jigkoh3@hotmail.com>
2017-01-22 00:09:03 +0700

    remove admin,postcode menu

--- modules/postcodes/client/config/postcodes.client.config.js
+++ modules/postcodes/client/config/postcodes.client.config.js
@@ -9,24 +9,24 @@
 
   function menuConfig(menuService) {
     // Set top bar menu items
-    menuService.addMenuItem('topbar', {
-      title: 'Postcodes',
-      state: 'postcodes',
-      type: 'dropdown',
-      roles: ['admin']
-    });
+    // menuService.addMenuItem('topbar', {
+    //   title: 'Postcodes',
+    //   state: 'postcodes',
+    //   type: 'dropdown',
+    //   roles: ['admin']
+    // });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'postcodes', {
-      title: 'List Postcodes',
-      state: 'postcodes.list'
-    });
+    // menuService.addSubMenuItem('topbar', 'postcodes', {
+    //   title: 'List Postcodes',
+    //   state: 'postcodes.list'
+    // });
 
     // Add the dropdown create item
-    menuService.addSubMenuItem('topbar', 'postcodes', {
-      title: 'Create Postcode',
-      state: 'postcodes.create',
-      roles: ['admin']
-    });
+    // menuService.addSubMenuItem('topbar', 'postcodes', {
+    //   title: 'Create Postcode',
+    //   state: 'postcodes.create',
+    //   roles: ['admin']
+    // });
   }
 }());
