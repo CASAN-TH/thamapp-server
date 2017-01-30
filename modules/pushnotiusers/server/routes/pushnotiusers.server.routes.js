01eccc4531cc404fa381aa79cf11ee87c97b8325
Sarawut Tana <mynameissarawut@gmail.com>
2017-01-30 13:03:46 +0700

    pushnotiusers close policy

--- modules/pushnotiusers/server/routes/pushnotiusers.server.routes.js
+++ modules/pushnotiusers/server/routes/pushnotiusers.server.routes.js
@@ -8,11 +8,11 @@ var pushnotiusersPolicy = require('../policies/pushnotiusers.server.policy'),
 
 module.exports = function(app) {
   // Pushnotiusers Routes
-  app.route('/api/pushnotiusers').all(pushnotiusersPolicy.isAllowed)
+  app.route('/api/pushnotiusers')//.all(pushnotiusersPolicy.isAllowed)
     .get(pushnotiusers.list)
     .post(pushnotiusers.create);
 
-  app.route('/api/pushnotiusers/:pushnotiuserId').all(pushnotiusersPolicy.isAllowed)
+  app.route('/api/pushnotiusers/:pushnotiuserId')//.all(pushnotiusersPolicy.isAllowed)
     .get(pushnotiusers.read)
     .put(pushnotiusers.update)
     .delete(pushnotiusers.delete);
