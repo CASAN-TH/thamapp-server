4786700890979be040eec88125888aab1755e14c
Bu4y <amnes1a@windowslive.com>
2017-02-02 09:50:27 +0700

    hide menu pushnotiusers

--- modules/pushnotiusers/client/config/pushnotiusers.client.config.js
+++ modules/pushnotiusers/client/config/pushnotiusers.client.config.js
@@ -9,24 +9,24 @@
 
   function menuConfig(menuService) {
     // Set top bar menu items
-    menuService.addMenuItem('topbar', {
-      title: 'Pushnotiusers',
-      state: 'pushnotiusers',
-      type: 'dropdown',
-      roles: ['admin']
-    });
+    // menuService.addMenuItem('topbar', {
+    //   title: 'Pushnotiusers',
+    //   state: 'pushnotiusers',
+    //   type: 'dropdown',
+    //   roles: ['admin']
+    // });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'pushnotiusers', {
-      title: 'List Pushnotiusers',
-      state: 'pushnotiusers.list'
-    });
+    // menuService.addSubMenuItem('topbar', 'pushnotiusers', {
+    //   title: 'List Pushnotiusers',
+    //   state: 'pushnotiusers.list'
+    // });
 
     // Add the dropdown create item
-    menuService.addSubMenuItem('topbar', 'pushnotiusers', {
-      title: 'Create Pushnotiuser',
-      state: 'pushnotiusers.create',
-      roles: ['admin']
-    });
+    // menuService.addSubMenuItem('topbar', 'pushnotiusers', {
+    //   title: 'Create Pushnotiuser',
+    //   state: 'pushnotiusers.create',
+    //   roles: ['admin']
+    // });
   }
 }());
