a9cb31f2d3b3f118eb0338ff5f7f309470690457
jigkoh3 <jigkoh3@hotmail.com>
2017-02-22 23:10:43 +0700

    1

--- modules/transports/client/config/transports.client.config.js
+++ modules/transports/client/config/transports.client.config.js
@@ -8,25 +8,25 @@
   menuConfig.$inject = ['Menus'];
 
   function menuConfig(menuService) {
-    // Set top bar menu items
-    menuService.addMenuItem('topbar', {
-      title: 'บริษัทขนส่ง',
-      state: 'transports',
-      type: 'dropdown',
-      roles: ['admin']
-    });
+    // // Set top bar menu items
+    // menuService.addMenuItem('topbar', {
+    //   title: 'บริษัทขนส่ง',
+    //   state: 'transports',
+    //   type: 'dropdown',
+    //   roles: ['admin']
+    // });
 
-    // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'transports', {
-      title: 'List Transports',
-      state: 'transports.list'
-    });
+    // // Add the dropdown list item
+    // menuService.addSubMenuItem('topbar', 'transports', {
+    //   title: 'List Transports',
+    //   state: 'transports.list'
+    // });
 
-    // Add the dropdown create item
-    menuService.addSubMenuItem('topbar', 'transports', {
-      title: 'Create Transport',
-      state: 'transports.create',
-      roles: ['admin']
-    });
+    // // Add the dropdown create item
+    // menuService.addSubMenuItem('topbar', 'transports', {
+    //   title: 'Create Transport',
+    //   state: 'transports.create',
+    //   roles: ['admin']
+    // });
   }
 }());
