301c7229a17424d7042c3d57a17fd379ac9e2d5c
วัจน์กร จงกลาง < >
2017-02-23 16:07:34 +0700

    accural

--- modules/accuralreceipts/client/config/accuralreceipts.client.config.js
+++ modules/accuralreceipts/client/config/accuralreceipts.client.config.js
@@ -2,31 +2,31 @@
   'use strict';
 
   angular
-    .module('accuralreceipts');
-  //   .run(menuConfig);
+    .module('accuralreceipts')
+    .run(menuConfig);
 
-  // menuConfig.$inject = ['Menus'];
+  menuConfig.$inject = ['Menus'];
 
-  // function menuConfig(menuService) {
-  //   // Set top bar menu items
-  //   menuService.addMenuItem('topbar', {
-  //     title: 'Accuralreceipts',
-  //     state: 'accuralreceipts',
-  //     type: 'dropdown',
-  //     roles: ['*']
-  //   });
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    menuService.addMenuItem('topbar', {
+      title: 'Accuralreceipts',
+      state: 'accuralreceipts',
+      type: 'dropdown',
+      roles: ['*']
+    });
 
-  //   // Add the dropdown list item
-  //   menuService.addSubMenuItem('topbar', 'accuralreceipts', {
-  //     title: 'List Accuralreceipts',
-  //     state: 'accuralreceipts.list'
-  //   });
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'accuralreceipts', {
+      title: 'List Accuralreceipts',
+      state: 'accuralreceipts.list'
+    });
 
-  //   // Add the dropdown create item
-  //   menuService.addSubMenuItem('topbar', 'accuralreceipts', {
-  //     title: 'Create Accuralreceipt',
-  //     state: 'accuralreceipts.create',
-  //     roles: ['user']
-  //   });
-  // }
+    // Add the dropdown create item
+    menuService.addSubMenuItem('topbar', 'accuralreceipts', {
+      title: 'Create Accuralreceipt',
+      state: 'accuralreceipts.create',
+      roles: ['user']
+    });
+  }
 }());
