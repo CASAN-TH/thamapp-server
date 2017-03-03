b3b5ab5a90b7e249594a8f4c23b7ca7706811de9
วัจน์กร จงกลาง < >
2017-03-03 11:04:24 +0700

    stock

--- modules/stocks/client/config/stocks.client.config.js
+++ modules/stocks/client/config/stocks.client.config.js
@@ -9,24 +9,24 @@
 
   function menuConfig(menuService) {
     // Set top bar menu items
-    // menuService.addMenuItem('topbar', {
-    //   title: 'Stocks',
-    //   state: 'stocks',
-    //   type: 'dropdown',
-    //   roles: ['*']
-    // });
+    menuService.addMenuItem('topbar', {
+      title: 'คลังสินค้า',
+      state: 'stocks',
+      type: 'dropdown',
+      roles: ['*']
+    });
 
-    // // Add the dropdown list item
-    // menuService.addSubMenuItem('topbar', 'stocks', {
-    //   title: 'List Stocks',
-    //   state: 'stocks.list'
-    // });
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'stocks', {
+      title: 'List Stocks',
+      state: 'stocks.list'
+    });
 
-    // // Add the dropdown create item
-    // menuService.addSubMenuItem('topbar', 'stocks', {
-    //   title: 'Create Stock',
-    //   state: 'stocks.create',
-    //   roles: ['user']
-    // });
+    // Add the dropdown create item
+    menuService.addSubMenuItem('topbar', 'stocks', {
+      title: 'Create Stock',
+      state: 'stocks.create',
+      roles: ['user']
+    });
   }
 }());
