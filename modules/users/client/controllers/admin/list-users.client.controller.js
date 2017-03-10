6a8157ac01767f61fe100c81e6bf5cbeffd2ec0d
วัจน์กร จงกลาง < >
2017-03-10 11:43:59 +0700

    list admin

--- modules/users/client/controllers/admin/list-users.client.controller.js
+++ modules/users/client/controllers/admin/list-users.client.controller.js
@@ -1,47 +1,61 @@
-'use strict';
+// 'use strict';
 
-angular.module('users.admin').controller('UserListController', ['$scope', '$filter', 'Admin',
-  function ($scope, $filter, Admin) {
-     var vm = this;
-     vm.users = Admin.query();
+// angular.module('users.admin').controller('UserListController', ['$scope', '$filter', 'Admin', 'Authentication',
+//   function ($scope, $filter, Admin, Authentication) {
+//      var vm = this;
+//      vm.authentication = Authentication;
+//      vm.users = Admin.query();
+//      console.log(vm.users);
 
-      vm.listuser = function (user) {
-      if (vm.authentication.user.roles[0] === 'admin') {
-        if (data.arstatus === 'confirmed') {
-          return true;
-        }
-      }
-    };
-     vm.listuser = function(user){
-       console.log(user.roles[0]);
-       if(user.roles[0] === 'user'){
-        return true;
-       }
-     }
-    // Admin.query(function (data) {
-    //   $scope.users = data;
-    //   $scope.buildPager();
-    // });
+//      vm.listuser = function(user){
+//        console.log(user.roles[0]);
+//        if(user.roles[0] === 'user'){
+//         return true;
+//        }
+//      }
+//     // Admin.query(function (data) {
+//     //   $scope.users = data;
+//     //   $scope.buildPager();
+//     // });
 
-    // $scope.buildPager = function () {
-    //   $scope.pagedItems = [];
-    //   $scope.itemsPerPage = 15;
-    //   $scope.currentPage = 1;
-    //   $scope.figureOutItemsToDisplay();
-    // };
+//     // $scope.buildPager = function () {
+//     //   $scope.pagedItems = [];
+//     //   $scope.itemsPerPage = 15;
+//     //   $scope.currentPage = 1;
+//     //   $scope.figureOutItemsToDisplay();
+//     // };
 
-    // $scope.figureOutItemsToDisplay = function () {
-    //   $scope.filteredItems = $filter('filter')($scope.users, {
-    //     $: $scope.search
-    //   });
-    //   $scope.filterLength = $scope.filteredItems.length;
-    //   var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
-    //   var end = begin + $scope.itemsPerPage;
-    //   $scope.pagedItems = $scope.filteredItems.slice(begin, end);
-    // };
+//     // $scope.figureOutItemsToDisplay = function () {
+//     //   $scope.filteredItems = $filter('filter')($scope.users, {
+//     //     $: $scope.search
+//     //   });
+//     //   $scope.filterLength = $scope.filteredItems.length;
+//     //   var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+//     //   var end = begin + $scope.itemsPerPage;
+//     //   $scope.pagedItems = $scope.filteredItems.slice(begin, end);
+//     // };
 
-    // $scope.pageChanged = function () {
-    //   $scope.figureOutItemsToDisplay();
-    // };
+//     // $scope.pageChanged = function () {
+//     //   $scope.figureOutItemsToDisplay();
+//     // };
+//   }
+// ]);
+
+(function () {
+  'use strict';
+
+  angular
+    .module('users.admin')
+    .controller('UserListController', UserListController);
+
+  UserListController.$inject = ['$scope', 'Admin', '$filter', 'Authentication'];
+
+  function UserListController($scope, Admin, $filter, Authentication) {
+    var vm = this;
+    vm.authentication = Authentication;
+    vm.users = Admin.query();
+
+    console.log(vm.users);
+
   }
-]);
+})();
