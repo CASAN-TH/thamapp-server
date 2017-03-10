61234922cc0ecfde6db5ae3cb4c7f9a07de97617
Bu4y <amnes1a@windowslive.com>
2017-03-10 15:20:41 +0700

    push noti returnorder

--- modules/returnorders/server/controllers/returnorders.server.controller.js
+++ modules/returnorders/server/controllers/returnorders.server.controller.js
@@ -7,21 +7,30 @@ var path = require('path'),
   mongoose = require('mongoose'),
   Returnorder = mongoose.model('Returnorder'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
-  _ = require('lodash');
+  _ = require('lodash'),
+  Pushnotiuser = mongoose.model('Pushnotiuser'),
+  request = require('request'),
+  pushNotiUrl = 'https://api.ionic.io/push/notifications',
+  pushNotiAuthenADM = { profile: 'dev', auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxZWM3YWNjZi1hYTNjLTQ2ZjUtYmMyNS1kODQ1MmQ2NDRlZmMifQ.Q3-2r2TL0Mq6Aq1JJSmUoTnh0LaoyMA-ZVuOylkJ7nI' },
+  pushNotiAuthenDEL = { profile: 'dev', auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMDYyYTMxMy1iYTdlLTQwYjYtOGM1Yy1jN2U5Y2M1N2QxZGIifQ.7jkqgdcB0kNUoQwCzH5AbCH1iIrjykMj2EyLHCx3rUs' },
+  pushNotiAuthenTRA = { profile: 'dev', auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MTVkMjNlNS1mMzVkLTRmNjEtOTcyMy01ZWIxNGZjMzFjYjkifQ.8E_6neuDDdMz1cqVPxcFuk7RuwB0Tu-ksdBC2ZnCs8Y' };
 
 /**
  * Create a Returnorder
  */
-exports.create = function(req, res) {
+exports.create = function (req, res) {
   var returnorder = new Returnorder(req.body);
   returnorder.user = req.user;
 
-  returnorder.save(function(err) {
+  returnorder.save(function (err) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
       });
     } else {
+      deliverCreateAllAdminStatusReturn();
+      deliverCreateAllTransportStatusReturn();
+      deliverCreateStatusReturn(returnorder);
       res.jsonp(returnorder);
     }
   });
@@ -30,7 +39,7 @@ exports.create = function(req, res) {
 /**
  * Show the current Returnorder
  */
-exports.read = function(req, res) {
+exports.read = function (req, res) {
   // convert mongoose document to JSON
   var returnorder = req.returnorder ? req.returnorder.toJSON() : {};
 
@@ -44,12 +53,12 @@ exports.read = function(req, res) {
 /**
  * Update a Returnorder
  */
-exports.update = function(req, res) {
+exports.update = function (req, res) {
   var returnorder = req.returnorder;
 
   returnorder = _.extend(returnorder, req.body);
 
-  returnorder.save(function(err) {
+  returnorder.save(function (err) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
@@ -63,10 +72,10 @@ exports.update = function(req, res) {
 /**
  * Delete an Returnorder
  */
-exports.delete = function(req, res) {
+exports.delete = function (req, res) {
   var returnorder = req.returnorder;
 
-  returnorder.remove(function(err) {
+  returnorder.remove(function (err) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
@@ -80,8 +89,8 @@ exports.delete = function(req, res) {
 /**
  * List of Returnorders
  */
-exports.list = function(req, res) {
-  Returnorder.find().sort('-created').populate('user').populate('items.product').populate('namedeliver').populate('transport').exec(function(err, returnorders) {
+exports.list = function (req, res) {
+  Returnorder.find().sort('-created').populate('user').populate('items.product').populate('namedeliver').populate('transport').exec(function (err, returnorders) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
@@ -95,7 +104,7 @@ exports.list = function(req, res) {
 /**
  * Returnorder middleware
  */
-exports.returnorderByID = function(req, res, next, id) {
+exports.returnorderByID = function (req, res, next, id) {
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).send({
@@ -115,3 +124,339 @@ exports.returnorderByID = function(req, res, next, id) {
     next();
   });
 };
+
+// status return
+function deliverCreateAllAdminStatusReturn() {
+  Returnorder.find().sort('-created').where('deliverystatus').equals('return').exec(function (err, reqReturs) {
+    if (err) {
+
+    } else {
+      Pushnotiuser.find().sort('-created').where('role').equals('admin').exec(function (err, admins) {
+        if (err) {
+
+        } else {
+          var admtokens = [];
+          admins.forEach(function (admin) {
+            admtokens.push(admin.device_token);
+          });
+
+          request({
+            url: pushNotiUrl,
+            auth: {
+              'bearer': pushNotiAuthenADM.auth
+            },
+            method: 'POST',
+            json: {
+              tokens: admtokens,
+              profile: pushNotiAuthenADM.profile,
+              notification: {
+                message: 'คุณมีรายการใบแจ้งคืนใหม่ ' + reqReturs.length + ' รายการ',
+                ios: { badge: reqReturs.length, sound: 'default' },
+                android: { data: { badge: reqReturs.length } }//{ badge: orders.length, sound: 'default' }
+              }
+            }
+          }, function (error, response, body) {
+            if (error) {
+              console.log('Error sending messages: ', error);
+            } else if (response.body.error) {
+              console.log('Error: ', response.body.error);
+            }
+          });
+        }
+      });
+    }
+  });
+
+
+}
+
+function deliverCreateAllTransportStatusReturn() {
+  Returnorder.find().sort('-created').where('deliverystatus').equals('return').exec(function (err, reqReturs) {
+    if (err) {
+
+    } else {
+      Pushnotiuser.find().sort('-created').where('role').equals('transporter').exec(function (err, trns) {
+        if (err) {
+
+        } else {
+          var trntokens = [];
+          trns.forEach(function (trn) {
+            trntokens.push(trn.device_token);
+          });
+
+          request({
+            url: pushNotiUrl,
+            auth: {
+              'bearer': pushNotiAuthenTRA.auth
+            },
+            method: 'POST',
+            json: {
+              tokens: trntokens,
+              profile: pushNotiAuthenTRA.profile,
+              notification: {
+                message: 'คุณมีรายการใบแจ้งคืนใหม่ ' + reqReturs.length + ' รายการ',
+                ios: { badge: reqReturs.length, sound: 'default' },
+                android: { data: { badge: reqReturs.length } }//{ badge: orders.length, sound: 'default' }
+              }
+            }
+          }, function (error, response, body) {
+            if (error) {
+              console.log('Error sending messages: ', error);
+            } else if (response.body.error) {
+              console.log('Error: ', response.body.error);
+            }
+          });
+        }
+      });
+    }
+  });
+
+
+}
+
+function deliverCreateStatusReturn(data) {
+  var me = '';
+  if(data && data.namedeliver){
+    me = data.namedeliver._id;
+  }else{
+    me = data.data;
+  }
+  Returnorder.find().sort('-created').where('deliverystatus').equals('return').exec(function (err, reqReturs) {
+    if (err) {
+
+    } else {
+      Pushnotiuser.find().sort('-created').where('role').equals('deliver').where('user_id').equals(me).exec(function (err, delivers) {
+        if (err) {
+
+        } else {
+          var dertokens = [];
+          delivers.forEach(function (deliver) {
+            dertokens.push(deliver.device_token);
+          });
+
+          request({
+            url: pushNotiUrl,
+            auth: {
+              'bearer': pushNotiAuthenDEL.auth
+            },
+            method: 'POST',
+            json: {
+              tokens: dertokens,
+              profile: pushNotiAuthenDEL.profile,
+              notification: {
+                message: 'คุณมีรายการใบแจ้งคืนใหม่ ' + reqReturs.length + ' รายการ',
+                ios: { badge: reqReturs.length, sound: 'default' },
+                android: { data: { badge: reqReturs.length } }//{ badge: orders.length, sound: 'default' }
+              }
+            }
+          }, function (error, response, body) {
+            if (error) {
+              console.log('Error sending messages: ', error);
+            } else if (response.body.error) {
+              console.log('Error: ', response.body.error);
+            }
+          });
+        }
+      });
+    }
+  });
+
+
+}
+
+
+// status response
+function statusResponseAllAdmin(data) {
+  Returnorder.find().sort('-created').where('deliverystatus').equals('response').exec(function (err, reqReturs) {
+    if (err) {
+
+    } else {
+      Pushnotiuser.find().sort('-created').where('role').equals('admin').exec(function (err, admins) {
+        if (err) {
+
+        } else {
+          var admtokens = [];
+          admins.forEach(function (admin) {
+            admtokens.push(admin.device_token);
+          });
+
+          request({
+            url: pushNotiUrl,
+            auth: {
+              'bearer': pushNotiAuthenADM.auth
+            },
+            method: 'POST',
+            json: {
+              tokens: admtokens,
+              profile: pushNotiAuthenADM.profile,
+              notification: {
+                message: 'คุณมีรายการยอมรับการส่งคืน ' + data.docno + ' จากบริษัทขนส่งแล้ว',
+                ios: { badge: reqReturs.length, sound: 'default' },
+                android: { data: { badge: reqReturs.length } }//{ badge: orders.length, sound: 'default' }
+              }
+            }
+          }, function (error, response, body) {
+            if (error) {
+              console.log('Error sending messages: ', error);
+            } else if (response.body.error) {
+              console.log('Error: ', response.body.error);
+            }
+          });
+        }
+      });
+    }
+  });
+
+
+}
+
+function statusResponseSingleDeliver(data) {
+  var me = '';
+  if(data && data.namedeliver){
+    me = data.namedeliver._id;
+  }else{
+    me = data;
+  }
+  Returnorder.find().sort('-created').where('deliverystatus').equals('response').exec(function (err, reqReturs) {
+    if (err) {
+
+    } else {
+      Pushnotiuser.find().sort('-created').where('role').equals('deliver').where('user_id').equals(me).exec(function (err, delivers) {
+        if (err) {
+
+        } else {
+          var dlrtokens = [];
+          delivers.forEach(function (deliver) {
+            dlrtokens.push(deliver.device_token);
+          });
+
+          request({
+            url: pushNotiUrl,
+            auth: {
+              'bearer': pushNotiAuthenDEL.auth
+            },
+            method: 'POST',
+            json: {
+              tokens: dlrtokens,
+              profile: pushNotiAuthenDEL.profile,
+              notification: {
+                message: 'รายการส่งคืน ' + data.docno + ' มีบริษัทขนส่งรับเรื่องแล้ว',
+                ios: { badge: reqReturs.length, sound: 'default' },
+                android: { data: { badge: reqReturs.length } }//{ badge: orders.length, sound: 'default' }
+              }
+            }
+          }, function (error, response, body) {
+            if (error) {
+              console.log('Error sending messages: ', error);
+            } else if (response.body.error) {
+              console.log('Error: ', response.body.error);
+            }
+          });
+        }
+      });
+    }
+  });
+
+
+}
+
+// status received
+function statusReceivedSingleTransport(data) {
+  var me = '';
+  if(data && data.transport){
+    me = data.transport._id;
+  }else{
+    me = data;
+  }
+  Returnorder.find().sort('-created').where('deliverystatus').equals('received').exec(function (err, reqReturs) {
+    if (err) {
+
+    } else {
+      Pushnotiuser.find().sort('-created').where('role').equals('transporter').where('user_id').equals(me).exec(function (err, trans) {
+        if (err) {
+
+        } else {
+          var trntokens = [];
+          trans.forEach(function (tran) {
+            trntokens.push(tran.device_token);
+          });
+
+          request({
+            url: pushNotiUrl,
+            auth: {
+              'bearer': pushNotiAuthenTRA.auth
+            },
+            method: 'POST',
+            json: {
+              tokens: trntokens,
+              profile: pushNotiAuthenTRA.profile,
+              notification: {
+                message: 'รายการ ' + data.docno + ' ส่งคืนเรียบร้อยแล้ว',
+                ios: { badge: reqReturs.length, sound: 'default' },
+                android: { data: { badge: reqReturs.length } }//{ badge: orders.length, sound: 'default' }
+              }
+            }
+          }, function (error, response, body) {
+            if (error) {
+              console.log('Error sending messages: ', error);
+            } else if (response.body.error) {
+              console.log('Error: ', response.body.error);
+            }
+          });
+        }
+      });
+    }
+  });
+
+
+}
+
+function statusReceivedSingleDeliver(data) {
+  var me = '';
+  if(data && data.namedeliver){
+    me = data.namedeliver._id;
+  }else{
+    me = data;
+  }
+  Returnorder.find().sort('-created').where('deliverystatus').equals('request').exec(function (err, reqReturs) {
+    if (err) {
+
+    } else {
+      Pushnotiuser.find().sort('-created').where('role').equals('deliver').where('user_id').equals(me).exec(function (err, delivers) {
+        if (err) {
+
+        } else {
+          var dlrtokens = [];
+          delivers.forEach(function (deliver) {
+            dlrtokens.push(deliver.device_token);
+          });
+
+          request({
+            url: pushNotiUrl,
+            auth: {
+              'bearer': pushNotiAuthenDEL.auth
+            },
+            method: 'POST',
+            json: {
+              tokens: dlrtokens,
+              profile: pushNotiAuthenDEL.profile,
+              notification: {
+                message: 'รายการ ' + data.docno + ' ส่งคืนเรียบร้อยแล้ว',
+                ios: { badge: reqReturs.length, sound: 'default' },
+                android: { data: { badge: reqReturs.length } }//{ badge: orders.length, sound: 'default' }
+              }
+            }
+          }, function (error, response, body) {
+            if (error) {
+              console.log('Error sending messages: ', error);
+            } else if (response.body.error) {
+              console.log('Error: ', response.body.error);
+            }
+          });
+        }
+      });
+    }
+  });
+
+
+}
