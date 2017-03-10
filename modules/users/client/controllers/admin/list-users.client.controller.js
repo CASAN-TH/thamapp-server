fcd103c4ffbe930e95d37e2d420008b63d6ed887
วัจน์กร จงกลาง < >
2017-03-10 11:28:50 +0700

    list admin

--- modules/users/client/controllers/admin/list-users.client.controller.js
+++ modules/users/client/controllers/admin/list-users.client.controller.js
@@ -2,30 +2,46 @@
 
 angular.module('users.admin').controller('UserListController', ['$scope', '$filter', 'Admin',
   function ($scope, $filter, Admin) {
-    Admin.query(function (data) {
-      $scope.users = data;
-      $scope.buildPager();
-    });
+     var vm = this;
+     vm.users = Admin.query();
 
-    $scope.buildPager = function () {
-      $scope.pagedItems = [];
-      $scope.itemsPerPage = 15;
-      $scope.currentPage = 1;
-      $scope.figureOutItemsToDisplay();
+      vm.listuser = function (user) {
+      if (vm.authentication.user.roles[0] === 'admin') {
+        if (data.arstatus === 'confirmed') {
+          return true;
+        }
+      }
     };
+     vm.listuser = function(user){
+       console.log(user.roles[0]);
+       if(user.roles[0] === 'user'){
+        return true;
+       }
+     }
+    // Admin.query(function (data) {
+    //   $scope.users = data;
+    //   $scope.buildPager();
+    // });
 
-    $scope.figureOutItemsToDisplay = function () {
-      $scope.filteredItems = $filter('filter')($scope.users, {
-        $: $scope.search
-      });
-      $scope.filterLength = $scope.filteredItems.length;
-      var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
-      var end = begin + $scope.itemsPerPage;
-      $scope.pagedItems = $scope.filteredItems.slice(begin, end);
-    };
+    // $scope.buildPager = function () {
+    //   $scope.pagedItems = [];
+    //   $scope.itemsPerPage = 15;
+    //   $scope.currentPage = 1;
+    //   $scope.figureOutItemsToDisplay();
+    // };
 
-    $scope.pageChanged = function () {
-      $scope.figureOutItemsToDisplay();
-    };
+    // $scope.figureOutItemsToDisplay = function () {
+    //   $scope.filteredItems = $filter('filter')($scope.users, {
+    //     $: $scope.search
+    //   });
+    //   $scope.filterLength = $scope.filteredItems.length;
+    //   var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+    //   var end = begin + $scope.itemsPerPage;
+    //   $scope.pagedItems = $scope.filteredItems.slice(begin, end);
+    // };
+
+    // $scope.pageChanged = function () {
+    //   $scope.figureOutItemsToDisplay();
+    // };
   }
 ]);
