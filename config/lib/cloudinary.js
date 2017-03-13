bf372a5f76048448d627eb1d423c64ee09dae393
วัจน์กร จงกลาง < >
2017-03-13 14:57:11 +0700

    cloudinay

--- config/lib/cloudinary.js
+++ config/lib/cloudinary.js
@@ -12,10 +12,17 @@ var cloudinary = require('cloudinary');
 
 // clodinary Thamapptest
 
+// cloudinary.config({
+//     cloud_name: 'hflvlav04',
+//     api_key: '677826662678962',
+//     api_secret: 'lwKtVO7Jp6IljNJ7YEZFoFLzmMQ'
+// });
+
+// cloudinary purchasetest
 cloudinary.config({
-    cloud_name: 'hflvlav04',
-    api_key: '677826662678962',
-    api_secret: 'lwKtVO7Jp6IljNJ7YEZFoFLzmMQ'
+    cloud_name: 'hpipugeai',
+    api_key: '924436473164388',
+    api_secret: '6GG8MTxmtxqHI_W1XXwS8Ixv78k'
 });
 
 module.exports.cloudinary = cloudinary;
