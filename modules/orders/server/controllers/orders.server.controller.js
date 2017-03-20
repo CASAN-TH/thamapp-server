26e3ae41229ef6ee8df490d81af258762037c2f7
Bu4y <amnes1a@windowslive.com>
2017-03-20 16:16:35 +0700

    sad

--- modules/orders/server/controllers/orders.server.controller.js
+++ modules/orders/server/controllers/orders.server.controller.js
@@ -170,7 +170,7 @@ exports.salereport = function (req, res, next) {
   var end = req.enddate;
   var startdate = new Date(req.startdate);
   var orderslist = req.orders ? req.orders : [];
-  var saleday = saleDate(orderslist);
+  var saleday = [];
   var saleprod = [];
 
   res.jsonp({ orders: orderslist, saleday: saleday, saleprod: saleprod });
