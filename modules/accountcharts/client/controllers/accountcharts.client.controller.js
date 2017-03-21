fadeaa7ed5cdd5099b7fece650bfac170fd3dd1f
nicknameismos <mynameissarawut@gmail.com>
2017-03-21 12:24:29 +0700

    edit form status

--- modules/accountcharts/client/controllers/accountcharts.client.controller.js
+++ modules/accountcharts/client/controllers/accountcharts.client.controller.js
@@ -8,7 +8,7 @@
 
   AccountchartsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'accountchartResolve'];
 
-  function AccountchartsController ($scope, $state, $window, Authentication, accountchart) {
+  function AccountchartsController($scope, $state, $window, Authentication, accountchart) {
     var vm = this;
 
     vm.authentication = Authentication;
@@ -17,37 +17,37 @@
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
-
-    // Remove existing Accountchart
-    function remove() {
-      if ($window.confirm('Are you sure you want to delete?')) {
-        vm.accountchart.$remove($state.go('accountcharts.list'));
-      }
-    }
-
-    // Save Accountchart
-    function save(isValid) {
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'vm.form.accountchartForm');
-        return false;
-      }
-
-      // TODO: move create/update logic to service
-      if (vm.accountchart._id) {
-        vm.accountchart.$update(successCallback, errorCallback);
-      } else {
-        vm.accountchart.$save(successCallback, errorCallback);
-      }
-
-      function successCallback(res) {
-        $state.go('accountcharts.list', {
-          accountchartId: res._id
-        });
-      }
-
-      function errorCallback(res) {
-        vm.error = res.data.message;
-      }
+  
+  // Remove existing Accountchart
+  function remove() {
+    if ($window.confirm('Are you sure you want to delete?')) {
+      vm.accountchart.$remove($state.go('accountcharts.list'));
     }
   }
+
+  // Save Accountchart
+  function save(isValid) {
+    if (!isValid) {
+      $scope.$broadcast('show-errors-check-validity', 'vm.form.accountchartForm');
+      return false;
+    }
+
+    // TODO: move create/update logic to service
+    if (vm.accountchart._id) {
+      vm.accountchart.$update(successCallback, errorCallback);
+    } else {
+      vm.accountchart.$save(successCallback, errorCallback);
+    }
+
+    function successCallback(res) {
+      $state.go('accountcharts.list', {
+        accountchartId: res._id
+      });
+    }
+
+    function errorCallback(res) {
+      vm.error = res.data.message;
+    }
+  }
+}
 }());
