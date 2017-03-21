d84c8c22c837a0ce2f72704a9721936a2a139280
nicknameismos <mynameissarawut@gmail.com>
2017-03-21 17:43:11 +0700

    audit log

--- modules/accountcharts/server/models/accountchart.server.model.js
+++ modules/accountcharts/server/models/accountchart.server.model.js
@@ -4,8 +4,8 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
+  auditLog = require('audit-log'),
   Schema = mongoose.Schema;
-
 /**
  * Accountchart Schema
  */
@@ -36,5 +36,15 @@ var AccountchartSchema = new Schema({
     ref: 'User'
   }
 });
+var pluginFn = auditLog.getPlugin('mongoose', { modelName: 'Accountchart', namePath: 'accountname' }); // setup occurs here 
+AccountchartSchema.plugin(pluginFn.handler);
 
+var pluginFn1 = auditLog.getPlugin('mongoose', { modelName: 'Accountchart', namePath: 'accountno' }); // setup occurs here 
+AccountchartSchema.plugin(pluginFn1.handler);
 mongoose.model('Accountchart', AccountchartSchema);
+
+var conn = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev';
+// console.log(conn);
+auditLog.addTransport('mongoose', { connectionString: conn });
+// either or both -- up to you where your messages are sent! 
+auditLog.addTransport('console');
