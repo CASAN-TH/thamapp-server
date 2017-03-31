f7bb3fa59abcf42b1ef140058ee4f3d177b1116d
nicknameismos <mynameissarawut@gmail.com>
2017-03-31 16:04:34 +0700

    led report

--- modules/payments/client/controllers/ledgerreport.client.controller.js
+++ modules/payments/client/controllers/ledgerreport.client.controller.js
@@ -1,21 +1,34 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('payments')
     .controller('LedgerreportController', LedgerreportController);
 
-  LedgerreportController.$inject = ['$scope'];
+  LedgerreportController.$inject = ['$scope', '$http', 'PaymentsService', 'Authentication'];
 
-  function LedgerreportController($scope) {
+  function LedgerreportController($scope, $http, PaymentsService, Authentication) {
     var vm = this;
+    vm.authentication = Authentication;
+    vm.listpayment = [];
+
+    var lastweek = new Date();
+    $scope.startDay = new Date(lastweek.getFullYear(), lastweek.getMonth(), lastweek.getDate() - 29);
+    $scope.endDay = new Date();
+
+    vm.getDay = function (startDay, endDay) {
+      $http.get('api/ledgers/' + startDay + '/' + endDay).success(function (response) {
+        console.log(response);
+        // console.log(response);
+        // if (response.orders.length === 0) {
+        //   alert('ไม่พบข้อมูล');
+        // }
+      }).error(function (err) {
+        console.log(err);
+      });
+    };
 
     // Ledgerreport controller logic
     // ...
-
-    init();
-
-    function init() {
-    }
   }
 })();
