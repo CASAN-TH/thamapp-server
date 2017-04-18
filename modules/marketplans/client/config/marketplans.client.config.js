1b8b089fae82c98769c38d73ff691ec3b076b9d4
moomam6934 <destinationpainbm@gmail.com>
2017-04-18 15:18:19 +0700

    edit config market

--- modules/marketplans/client/config/marketplans.client.config.js
+++ modules/marketplans/client/config/marketplans.client.config.js
@@ -10,7 +10,7 @@
   function menuConfig(menuService) {
     // Set top bar menu items
     menuService.addMenuItem('topbar', {
-      title: 'Marketplans',
+      title: 'ตลาดนัดธรรมชาติ',
       state: 'marketplans',
       type: 'dropdown',
       roles: ['*']
