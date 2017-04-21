3512fe42332c3ad1ef9dcad206a59b104632c388
วัจน์กร จงกลาง < >
2017-04-21 15:55:20 +0700

    edit marketplan

--- modules/marketplans/server/controllers/marketplans.server.controller.js
+++ modules/marketplans/server/controllers/marketplans.server.controller.js
@@ -90,7 +90,7 @@ exports.delete = function (req, res) {
  * List of Marketplans
  */
 exports.list = function (req, res) {
-  Marketplan.find({ enddate: { $gte: new Date() } }).sort('-created').populate('user', 'displayName').exec(function (err, marketplans) {
+  Marketplan.find({ enddate: { $gte: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 1) } }).sort('-created').populate('user', 'displayName').exec(function (err, marketplans) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
