fad6a6bfc23d1966591886300b56ae8db5a9de4f
nicknameismos <mynameissarawut@gmail.com>
2017-04-26 19:43:56 +0700

    add chat & chatroom

---
+++ modules/chat/client/controllers/chat.client.controller.js
@@ -0,0 +1,99 @@
+'use strict';
+
+// Create the 'chat' controller
+angular.module('chat').controller('ChatController', ['$scope', '$location', 'Authentication', 'Socket', 'Admin', 'ChatroomsService',
+  function ($scope, $location, Authentication, Socket, Admin, ChatroomsService) {
+    // Create a messages array
+    $scope.authenID = Authentication.user._id;
+    $scope.username = Authentication.user.username;
+    $scope.messages = [];
+    Admin.query(function (data) {
+      $scope.users = data;
+    });
+    $scope.chatlist = ChatroomsService.query();
+    // $scope.userSelect = $scope.userSelect ? $scope.userSelect : {};
+    // If user is not signed in then redirect back home
+    if (!Authentication.user) {
+      $location.path('/');
+    }
+
+    // Make sure the Socket is connected
+    if (!Socket.socket) {
+      Socket.connect();
+    }
+    $scope.createGroup = function (user) {
+      $scope.userSelect = user;
+      console.log(user);
+
+      var data = {
+        name: Authentication.user.username + '' + user.username,
+        type: 'P',
+        users: [Authentication.user, user],
+        user: Authentication.user
+      };
+      Socket.emit('createroom', data);
+      $scope.chatlist = ChatroomsService.query();
+    };
+
+    // Add an event listener to the 'invite' event
+    Socket.on('invite', function (data) {
+      console.log('invite : ' + data);
+      Socket.emit('join', data);
+    });
+
+    // Add an event listener to the 'joinsuccess' event
+    Socket.on('joinsuccess', function (data) {
+      $scope.room = data;
+      console.log('joinsuccess : ' + data);
+      // data.text = 'hello';
+      // Socket.emit('chatMessage', data);
+    });
+
+    // Add an event listener to the 'chatMessage' event
+    Socket.on('chatMessage', function (data) {
+      $scope.room = data;
+    });
+
+    // Create a controller method for sending messages
+    $scope.sendMessage = function () {
+      // Create a new message object
+      // var message = {
+      //   text: this.messageText
+      // };
+      //$scope.room.text = this.messageText;
+      if(!$scope.room.messages){
+        $scope.room.messages = [];
+        $scope.room.messages.unshift({
+          type : 'message',
+          created : Date.now(),
+          profileImageURL : Authentication.user.profileImageURL,
+          username : Authentication.user.username,
+          text : this.messageText
+        });
+      }else{
+        $scope.room.messages.unshift({
+          type : 'message',
+          created : Date.now(),
+          profileImageURL : Authentication.user.profileImageURL,
+          username : Authentication.user.username,
+          text : this.messageText
+        });
+      }
+      
+
+      // Emit a 'chatMessage' message event
+      Socket.emit('chatMessage', $scope.room);
+
+      // Clear the message text
+      this.messageText = '';
+    };
+    $scope.roomsername = function (data) {
+      $scope.room = data;
+    };
+
+    // Remove the event listener when the controller instance is destroyed
+    $scope.$on('$destroy', function () {
+      Socket.removeListener('chatMessage');
+    });
+  }
+]);
---
+++ modules/chat/server/sockets/chat.server.socket.config.js
@@ -0,0 +1,98 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+var path = require('path'),
+  mongoose = require('mongoose'),
+  Chatroom = mongoose.model('Chatroom'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  _ = require('lodash');
+
+// Create the chat configuration
+module.exports = function (io, socket) {
+  // Emit the status event when a new socket client is connected
+  // io.emit('chatMessage', {
+  //   type: 'status',
+  //   text: 'Is now connected',
+  //   created: Date.now(),
+  //   profileImageURL: socket.request.user.profileImageURL,
+  //   username: socket.request.user.username
+  // });
+
+  socket.join(socket.request.user.username);
+
+  // create room
+  socket.on('createroom', function (data) {
+    // console.log('createroom Data : ' + data);
+    var chatroom = new Chatroom(data);
+    chatroom.user = socket.request.user;
+
+    chatroom.save(function (err) {
+      if (err) {
+        // return res.status(400).send({
+        //   message: errorHandler.getErrorMessage(err)
+        // });
+        // console.log('error : ' + JSON.stringify(err));
+      } else {
+        // console.log('success' + JSON.stringify(chatroom));
+        data.users.forEach(function (user) {
+          // console.log('success' + JSON.stringify(chatroom));
+          // console.log(JSON.stringify(user));
+          io.sockets.in(user.username).emit('invite', data);
+        });
+        // res.jsonp(chatroom);
+      }
+    });
+  });
+
+
+  socket.on('join', function (data) {
+    // console.log('join : ' + JSON.stringify(data));
+    socket.join(data.name);
+    io.sockets.in(data.name).emit('joinsuccess', data);
+  });
+
+  // Send a chat messages to all connected sockets when a message is received
+  socket.on('chatMessage', function (data) {
+    // console.log('chatMessage : ' + JSON.stringify(message));
+    socket.join(data.name);
+
+    io.sockets.in(data.name).emit('chatMessage', data);
+
+    // Chatroom.findById(data._id).populate('user', 'displayName').exec(function (err, chatroom) {
+    //   if (err) {
+    //     console.log(err);
+    //   } else if (!chatroom) {
+    //     console.log('Chatroom is invalid');
+    //   }
+    //   chatroom = _.extend(chatroom, data);
+    //   chatroom.save(function (err) {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       // Emit the 'chatMessage' event
+    //       io.sockets.in(data.name).emit('chatMessage', data);
+    //     }
+    //   });
+    // });
+
+
+
+    // message.type = 'message';
+    // message.created = Date.now();
+    // message.profileImageURL = socket.request.user.profileImageURL;
+    // message.username = socket.request.user.username;
+
+
+  });
+
+  // Emit the status event when a socket client is disconnected
+  // socket.on('disconnect', function () {
+  //   io.emit('chatMessage', {
+  //     type: 'status',
+  //     text: 'disconnected',
+  //     created: Date.now(),
+  //     username: socket.request.user.username
+  //   });
+  // });
+};
