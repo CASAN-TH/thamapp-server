7355709995febad6f44d2cc177d138da977b87ba
วัจน์กร จงกลาง < >
2017-05-04 17:27:15 +0700

    quize

--- modules/quizzes/client/controllers/list-quizzes.client.controller.js
+++ modules/quizzes/client/controllers/list-quizzes.client.controller.js
@@ -5,13 +5,25 @@
     .module('quizzes')
     .controller('QuizzesListController', QuizzesListController);
 
-  QuizzesListController.$inject = ['QuizzesService', '$window', 'Authentication'];
+  QuizzesListController.$inject = ['QuizzesService', '$window', 'Authentication', '$state'];
 
-  function QuizzesListController(QuizzesService, $window, Authentication) {
+  function QuizzesListController(QuizzesService, $window, Authentication, $state) {
     var vm = this;
     vm.authentication = Authentication;
     vm.quizzes = QuizzesService.query();
 
+    function arrayObjectIndexOf(myArray, searchTerm, property) {
+      for (var i = 0, len = myArray.length; i < len; i++) {
+        if (myArray[i][property] === searchTerm) return i;
+      }
+      return -1;
+    }
+
+    vm.isNever = function (itm) {
+      // alert(itm.users);
+      // alert(arrayObjectIndexOf(itm.users, vm.authentication.user, '_id') === -1);
+        return arrayObjectIndexOf(itm.users, vm.authentication.user._id, '_id') === -1;
+    };
 
     vm.remove = function (itm) {
       if ($window.confirm('คุณต้องการลบคำถามนี้ ?')) {
