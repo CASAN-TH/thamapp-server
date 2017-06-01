c5442eb0d8f5086598309adc400baa0b431466ae
Bu4y <amnes1a@windowslive.com>
2017-06-01 12:57:11 +0700

    env > default.js

--- config/env/default.js
+++ config/env/default.js
@@ -23,7 +23,7 @@ module.exports = {
     secure: false
   },
   // sessionSecret should be changed for security measures and concerns
-  sessionSecret: process.env.SESSION_SECRET || 'MEAN1234',
+  sessionSecret: process.env.SESSION_SECRET || 'MEAN',
   // sessionKey is set to the generic sessionId key used by PHP applications
   // for obsecurity reasons
   sessionKey: 'sessionId',
