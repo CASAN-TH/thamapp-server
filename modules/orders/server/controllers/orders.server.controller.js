3c5c3e2b2ebe4c8bbc753a5b8d46ac0095cda598
jigkoh3 <jigkoh3@hotmail.com>
2017-06-02 11:33:47 +0700

    ่

--- modules/orders/server/controllers/orders.server.controller.js
+++ modules/orders/server/controllers/orders.server.controller.js
@@ -604,10 +604,10 @@ function sendNewdeliverOrder(order_location) {
       var delivertokens = [];
       delivers.forEach(function (deliver) {
        
-        //console.log(deliver.user.address);
+        console.log(deliver.user.address);
         if (order_location && deliver.user.address && deliver.user.address.sharelocation) {
           var dist = getDistanceFromLatLonInKm(order_location.latitude, order_location.longitude, deliver.user.address.sharelocation.latitude, deliver.user.address.sharelocation.longitude);
-          //console.log('------------- ' + dist + ' km. -------------')
+          console.log('------------- ' + dist + ' km. -------------')
           if (dist <= minDistance) {
             delivertokens.push(deliver.device_token);
           }
