0292f5d7baa8cb7f26084721410ab84218d93ad8
jigkoh3 <jigkoh3@hotmail.com>
2017-06-02 11:17:34 +0700

    console.log(deliver.user.address);

--- modules/orders/server/controllers/orders.server.controller.js
+++ modules/orders/server/controllers/orders.server.controller.js
@@ -610,6 +610,7 @@ function sendNewdeliverOrder(order_location) {
         //     }
         //   });
         // }
+        console.log(deliver.user.address);
         if (order_location && deliver.user.address && deliver.user.address.sharelocation) {
           var dist = getDistanceFromLatLonInKm(order_location.latitude, order_location.longitude, deliver.user.address.sharelocation.latitude, deliver.user.address.sharelocation.longitude);
           console.log('------------- ' + dist + ' km. -------------')
