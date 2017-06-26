1988cfd3079e2ca89a853f5b1f435b419a07e11d
Bu4y <amnes1a@windowslive.com>
2017-06-26 18:20:55 +0700

    update

--- modules/orders/server/routes/orders.server.routes.js
+++ modules/orders/server/routes/orders.server.routes.js
@@ -22,7 +22,7 @@ module.exports = function (app) {
     .get(ordersPolicy.isAllowed, orders.confirmed, orders.confirmedNearBy, orders.wait, orders.accept, orders.reject, orders.rejectNearBy, orders.complete, orders.cancel, orders.listorderv2);
   app.route('/api/listorder/web')//.all(ordersPolicy.isAllowed)
     .get(ordersPolicy.isAllowed, orders.confirmed, orders.confirmedNearBy, orders.wait, orders.accept, orders.reject, orders.rejectNearBy, orders.complete, orders.cancel, orders.listorderweb)
-    .post(users.requiresLoginToken, ordersPolicy.isAllowed, orders.create);
+    .post(users.requiresLoginToken, ordersPolicy.isAllowed,orders.adminCreate, orders.create);
   app.route('/api/listorder/web/:orderId')//.all(ordersPolicy.isAllowed)
     .get(ordersPolicy.isAllowed, orders.read)
     .put(users.requiresLoginToken, ordersPolicy.isAllowed, orders.update)
