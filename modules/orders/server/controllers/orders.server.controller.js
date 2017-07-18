5a79e3804bab09902df975099fe7e84e080761ca
Bu4y <amnes1a@windowslive.com>
2017-07-18 14:18:48 +0700

    new mdw

--- modules/orders/server/controllers/orders.server.controller.js
+++ modules/orders/server/controllers/orders.server.controller.js
@@ -218,43 +218,49 @@ exports.checkDeliver = function (req, res, next) {
 };
 
 exports.findOldDeliver = function (req, res, next) {
-  if (req.olddeliver) {
-    Pushnotiuser.find().sort('-created').where('role').equals('deliver').populate('user').exec(function (err, delivers) {
-      if (err) {
+  var order = new Order(req.body);
 
-      } else {
-        var delivertokens = [];
-        var usernearby = [];
-        // var delivertokens2 = [];
-        // var delivertokensOther = [];
-        if (delivers.length > 0) {
-
-          delivers.forEach(function (deliver) {
-            if (deliver.user._id.toString() === req.olddeliver._id.toString()) {
-
-
-              //console.log(deliver.user.address);
-
-              if (usernearby.indexOf(deliver.user._id) === -1) {
-                usernearby.push(deliver.user._id);
-              }
-              if (delivertokens.indexOf(deliver.device_token) === -1) {
-                delivertokens.push(deliver.device_token);
-              }
-            }
-
-            //delivertokens.push(deliver.device_token);
-          });
-          req.tokens = delivertokens;
-          req.usernearby = usernearby;
-          next();
-        } else {
-          next();
-        }
-      }
-    });
-  } else {
+  if (order.deliverystatus === 'complete') {
     next();
+  } else {
+    if (req.olddeliver) {
+      Pushnotiuser.find().sort('-created').where('role').equals('deliver').populate('user').exec(function (err, delivers) {
+        if (err) {
+
+        } else {
+          var delivertokens = [];
+          var usernearby = [];
+          // var delivertokens2 = [];
+          // var delivertokensOther = [];
+          if (delivers.length > 0) {
+
+            delivers.forEach(function (deliver) {
+              if (deliver.user._id.toString() === req.olddeliver._id.toString()) {
+
+
+                //console.log(deliver.user.address);
+
+                if (usernearby.indexOf(deliver.user._id) === -1) {
+                  usernearby.push(deliver.user._id);
+                }
+                if (delivertokens.indexOf(deliver.device_token) === -1) {
+                  delivertokens.push(deliver.device_token);
+                }
+              }
+
+              //delivertokens.push(deliver.device_token);
+            });
+            req.tokens = delivertokens;
+            req.usernearby = usernearby;
+            next();
+          } else {
+            next();
+          }
+        }
+      });
+    } else {
+      next();
+    }
   }
 
 };
