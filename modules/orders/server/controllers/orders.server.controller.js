f77a6000c3a30c8113f6ee3c9cf02a44e2dbfe93
Bu4y <amnes1a@windowslive.com>
2017-07-18 10:52:37 +0700

    noti billoy

--- modules/orders/server/controllers/orders.server.controller.js
+++ modules/orders/server/controllers/orders.server.controller.js
@@ -176,7 +176,38 @@ exports.checkDeliver = function (req, res, next) {
             });
             if (deliver2.length > 0) {
               req.olddeliver = deliver2[0];
-              next();
+              Pushnotiuser.find({ user: req.olddeliver }).sort('-created').where('role').equals('deliver').populate('user').exec(function (err, delivers) {
+                if (err) {
+
+                } else {
+                  var delivertokens = [];
+                  var usernearby = [];
+                  // var delivertokens2 = [];
+                  // var delivertokensOther = [];
+                  if (delivers.length > 0) {
+
+                    delivers.forEach(function (deliver) {
+
+                      //console.log(deliver.user.address);
+
+                      if (usernearby.indexOf(deliver.user._id) === -1) {
+                        usernearby.push(deliver.user._id);
+                      }
+                      if (delivertokens.indexOf(deliver.device_token) === -1) {
+                        delivertokens.push(deliver.device_token);
+                      }
+
+
+                      //delivertokens.push(deliver.device_token);
+                    });
+                    req.tokens = delivertokens;
+                    req.usernearby = usernearby;
+                    next();
+                  } else {
+                    next();
+                  }
+                }
+              });
             } else {
               next();
             }
