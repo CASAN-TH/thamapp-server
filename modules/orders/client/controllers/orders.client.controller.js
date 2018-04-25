1998872066a94974b0d0cb9dbcf1a2a98888c102
jigkoh3 <jigkoh3@hotmail.com>
2018-04-26 00:07:37 +0700

    sdfsd

--- modules/orders/client/controllers/orders.client.controller.js
+++ modules/orders/client/controllers/orders.client.controller.js
@@ -70,6 +70,14 @@
       }
     };
 
+    $scope.printDiv = function(divName) {
+      var printContents = document.getElementById(divName).innerHTML;
+      var popupWin = window.open('', '_blank', 'width=300,height=300');
+      popupWin.document.open();
+      popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
+      popupWin.document.close();
+    };
+
     if (vm.order.items) {
       vm.order.items = vm.order.items;
     } else {
