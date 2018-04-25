d6240a55f3a978c5ca3c5b5848edd91c9fbfe697
jigkoh3 <jigkoh3@hotmail.com>
2018-04-26 00:29:30 +0700

    xcvx

--- modules/orders/client/controllers/orders.client.controller.js
+++ modules/orders/client/controllers/orders.client.controller.js
@@ -74,7 +74,7 @@
       var printContents = document.getElementById(divName).innerHTML;
       var popupWin = window.open('', '_blank', 'width=300,height=300');
       popupWin.document.open();
-      popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
+      popupWin.document.write('<html><head><link rel="stylesheet"  href="lib/bootstrap/dist/css/bootstrap.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
       popupWin.document.close();
     };
 
